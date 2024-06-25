7e729886347a4f558f3320355026acf2c016c3c6
ZweThuta <hermez27zz@gmail.com>
2024-06-25 08:40:19 +0630

    MySQL Database

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
 const express = require("express");
-const path = require("path")
-const bodyParser = require("body-parser")
+const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
-const userRoutes = require("./routes/user")
-const {adminRoutes} = require("./routes/admin")
+const userRoutes = require("./routes/user");
+const { adminRoutes } = require("./routes/admin");
 
-app.set("view engine","ejs");
-app.set("views","views");
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.use(express.static(path.join(__dirname,"public")))
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/post", (req, res, next)=>{
-    console.log("Post Middleware");
-    next();
-})
-app.use((req, res, next)=>{
-    console.log("Parent Middleware");
-    next();
-})
-app.use("/admin",(req,res, next)=>{
-    console.log("Admin Middleware approved")
-    next();
-})
-app.use(userRoutes); 
-app.use("/admin",adminRoutes);
-
-
+app.use("/post", (req, res, next) => {
+  console.log("Post Middleware");
+  next();
+});
+app.use((req, res, next) => {
+  console.log("Parent Middleware");
+  next();
+});
+app.use("/admin", (req, res, next) => {
+  console.log("Admin Middleware approved");
+  next();
+});
+app.use(userRoutes);
+app.use("/admin", adminRoutes);
 
 app.listen(8080);
--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const path = require("path");
 const router = express.Router();
 const postController = require("../controllers/posts")
 
-router.get("/", postController.renderHomePage );
+router.get("/", postController.getPosts );
 router.get("/post/:postId", postController.getPost)
 // router.get("/post", (req, res) => {
 //   res.sendFile(path.join(__dirname, "..", "views", "postpage.html"));
